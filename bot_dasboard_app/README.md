1e1976a116ad41b15a108c356c327cd84d49662a
Andzzio <andzzio924@gmail.com>
2025-12-07 23:41:06 -0500

    Initial commit

---
+++ bot_dasboard_app/README.md
@@ -0,0 +1,3 @@
+# bot_dasboard_app
+
+A new Flutter project.
---
+++ bot_dasboard_app/android/app/src/main/kotlin/com/example/bot_dasboard_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bot_dasboard_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
